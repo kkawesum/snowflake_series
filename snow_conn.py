0890cc393c347a6df90498fe78a25bd514f7c367
Kislay Srivastava <KislayS@mymiratech.com>
2023-03-02 01:31:10 +0530

    test dml+dql

--- snow_conn.py
+++ snow_conn.py
@@ -12,11 +12,29 @@ try:
     row = cs.fetchone()
     print('connector version',row[0])
     print('creating objects')
-    ddl_sql = 'CREATE WAREHOUSE IF NOT EXISTS project_warehouse'
-    'CREATE DATABASE IF NOT EXISTS project_database'
-    'USE DATABASE project_database'
-    'CREATE SCHEMA IF NOT EXISTS project_schema'
+    sql = 'CREATE WAREHOUSE IF NOT EXISTS project_warehouse'
+    cs.execute(sql)
+    sql = 'CREATE DATABASE IF NOT EXISTS project_database'
+    cs.execute(sql)
+    sql= 'USE DATABASE project_database'
+    cs.execute(sql)
+    sql = 'CREATE SCHEMA IF NOT EXISTS project_schema'
+           
+    print('DDL done..')
+    cs.execute(sql)
+    print('start DML')
+    
+    sql = 'CREATE OR REPLACE TABLE project_database.project_schema.project_comments ( ID integer, comments string)'
+    cs.execute(sql)
+    sql = "Insert Into project_database.project_schema.project_comments (ID, comments) Values (777,'MYCOMMENTS')"
+
+    cs.execute(sql)
+    cs.execute("SELECT * FROM project_database.project_schema.project_comments")
+    row=cs.fetchone()
+    print('Row 1 :',row)
     print('done')
+
+    
     
 finally:
     cs.close()
